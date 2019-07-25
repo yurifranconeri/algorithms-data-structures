fb4bf8639e78b792ded64a3157901ddda429dcd0
Yuri Franconeri <yurifr@microsoft.com>
2019-07-25 08:17:05 -0300

    Created LinearSearch Algorithm with with Unit Test

---
+++ LinearSearch/LinearSearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearSearch
+{
+    public static class LinearSearch
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static int Search(int[] arrayValues, int valueToFind)
+        {
+            for (int index = 0; index < arrayValues.Length; index++)
+            {
+                if (arrayValues[index] == valueToFind)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ LinearSearchAlgorithmUnitTest/LinearSerchAlgorithmUnitTest.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinearSearch;
+
+namespace LinearSearchAlgorithmUnitTest
+{
+    [TestClass]
+    public class LinearSerchAlgorithmUnitTest
+    {
+        [TestMethod]
+        public void FindAValueOnArray()
+        {
+            var inputArray = new int[]{10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
+            var valueToFind = 110;
+            var valueIndex = 6;
+
+            var foundIndexValue=LinearSearch.LinearSearch.Search(inputArray, valueToFind);
+            Assert.AreEqual(foundIndexValue, valueIndex);
+        }
+
+        [TestMethod]
+        public void NotFindAValueOnArray()
+        {
+            var inputArray = new int[] { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
+            var valueToFind = 26;
+            var valueNotFoundIndex = -1;
+            
+            var foundIndexValue = LinearSearch.LinearSearch.Search(inputArray, valueToFind);
+            Assert.AreEqual(foundIndexValue, valueNotFoundIndex);
+        }
+    }
+}
